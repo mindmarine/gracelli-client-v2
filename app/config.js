1e217400a4426f25acbc68d829982468aa55da62
mindmarine <alex.drogeanu@gmail.com>
2021-08-12 10:42:16 -0400

    Added config file pointing to heroku deployment

--- app/config.js
+++ app/config.js
@@ -1,6 +1,7 @@
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  // production: '<replace-with-heroku-url>',
+  production: 'https://gracelli.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
